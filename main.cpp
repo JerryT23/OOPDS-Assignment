f01f72398cf8e1a1dde1ab4871d6026bbfd09093
Jerry <kahle040923@gmail.com>
2024-12-03 21:26:13 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+main(){
+    int iterations = 100;
+    int width = 10;
+    int height = 10;
+    char battleship = '*';
+    char cruiser = '$';
+    char destroyer = '#';
+    char frigate = '@';
+    char carvette = '&';
+    char amphibious = '~';
+
+
+
+}
