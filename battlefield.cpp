5948b06049d958f54b7f00d7252cf08166990e28
NCCHANG <changhoehin2005@gmail.com>
2024-12-06 23:07:34 +0800

    added init(unfinished)

--- battlefield.cpp
+++ battlefield.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 // 1. vectors, queues and linked lists is not allowed in this assignment
@@ -6,23 +7,34 @@ using namespace std;
 //    INHERITANCE, POLYMORPHISM, OPERATOR OVERLOADING, MOVE
 //    SEMANTICS and any number of C++ object oriented features
 
+class Game {
+    private:
+        int width;
+        int height;
+        int iterations;
+    public:
+    void init() {
+        
+    }
+};
+
 void display_battlefield(int position_ship1, int position_ship2, int position_ship3, int position_ship4, int position_ship5, int position_ship6){
-    const int iterations = 100;
+    // const int iterations = 100;
 
-    const int width = 10;
-    const int height = 10;
+    // const int width = 10;
+    // const int height = 10;
 
-    char battlefield[height][width];
+    // char battlefield[height][width];
 
-    char battleship = '*';
-    char cruiser = '$';
-    char destroyer = '#';
-    char frigate = '@';
-    char carvette = '&';
-    char amphibious = '~';
+    // char battleship = '*';
+    // char cruiser = '$';
+    // char destroyer = '#';
+    // char frigate = '@';
+    // char carvette = '&';
+    // char amphibious = '~';
 
      //Initialize battlefield
-    for (int i = 0; i < height; i++)
-        for (int j = 0; j < width; j++)
-            battlefield[i][j] = '0';
+    // for (int i = 0; i < height; i++)
+    //     for (int j = 0; j < width; j++)
+    //         battlefield[i][j] = '0';
 }
--- main.cpp
+++ main.cpp
@@ -1,17 +1,6 @@
 #include <iostream>
 using namespace std;
 
-main(){
-    int iterations = 100;
-    int width = 10;
-    int height = 10;
-    char battleship = '*';
-    char cruiser = '$';
-    char destroyer = '#';
-    char frigate = '@';
-    char carvette = '&';
-    char amphibious = '~';
-
-
-
+int main(){
+    
 }
