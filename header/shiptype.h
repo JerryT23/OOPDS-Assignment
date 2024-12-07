7184cdc4d9a0ca83c01c23c08e69cff64fbbaeca
NCCHANG <changhoehin2005@gmail.com>
2024-12-07 14:54:36 +0800

    added ship type class

--- header/shiptype.h
+++ header/shiptype.h
@@ -5,4 +5,34 @@ class Battleship {
     int shipPositionX;
     int shipPositionY;
 };
+class Cruiser {
+    static char symbol;
+    int shipPositionX;
+    int shipPositionY;
+};
+class Destroyer {
+    static char symbol;
+    int shipPositionX;
+    int shipPositionY;
+};
+class Frigate {
+    static char symbol;
+    int shipPositionX;
+    int shipPositionY;
+};
+class Corvette {
+    static char symbol;
+    int shipPositionX;
+    int shipPositionY;
+};
+class Amphibious {
+    static char symbol;
+    int shipPositionX;
+    int shipPositionY;
+};
+class Supership {
+    static char symbol; //what symbol oh?
+    int shipPositionX;
+    int shipPositionY;
+};
 #endif
