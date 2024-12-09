3e3f3536f2b4054b6e00161142363a91d24ccba4
NCCHANG <changhoehin2005@gmail.com>
2024-12-09 21:59:57 +0800

    added amount attribute in ship type

--- header/shiptype.h
+++ header/shiptype.h
@@ -2,36 +2,43 @@
 #define SHIPTYPE_H
 class Battleship {
     static char symbol;
+    static int amount;
     int shipPositionX;
     int shipPositionY;
 };
 class Cruiser {
     static char symbol;
+    static int amount;
     int shipPositionX;
     int shipPositionY;
 };
 class Destroyer {
     static char symbol;
+    static int amount;
     int shipPositionX;
     int shipPositionY;
 };
 class Frigate {
     static char symbol;
+    static int amount;
     int shipPositionX;
     int shipPositionY;
 };
 class Corvette {
     static char symbol;
+    static int amount;
     int shipPositionX;
     int shipPositionY;
 };
 class Amphibious {
     static char symbol;
+    static int amount;
     int shipPositionX;
     int shipPositionY;
 };
 class Supership {
     static char symbol; //what symbol oh?
+    static int amount;
     int shipPositionX;
     int shipPositionY;
 };
