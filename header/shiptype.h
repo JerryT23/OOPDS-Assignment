6d8f23f3efea135e2b2dec2cd0d112d816640a2a
NCCHANG <changhoehin2005@gmail.com>
2024-12-20 15:19:13 +0800

    req 2

--- header/shiptype.h
+++ header/shiptype.h
@@ -4,31 +4,46 @@ class Ship {
     int shipPositionX;
     int shipPositionY;
     public:
-    virtual void looks() = 0;
-    virtual void moves() = 0;
-    virtual void shoots() = 0;
-    virtual void idkyet() = 0;
+    virtual void look() = 0;
+    virtual void move() = 0;
+    virtual void shoot() = 0;
+    virtual void ram() = 0;
 };
 
 //no overlapping method and attribute for subclasses
 
 class SeeingRobot : public Ship {
-   
+   void look() {
+    //implement look
+   }
 };
 
 class MovingShip : public Ship {
-   
+   //implement look
 };
 
 class ShootingShip : public Ship {
-
+    //implement look
 };
 
 class RamShip : public Ship {
+    //implement look
+};
+
+class Battleship : public MovingShip, public SeeingRobot, public ShootingShip{
 
 };
 
-class Battleship : 
+class Cruiser : public SeeingRobot, public MovingShip, public RamShip {
+
+};
+
+class Destroyer : public MovingShip, public SeeingRobot, public ShootingShip, public RamShip {
+
+};
+class Frigate : public ShootingShip, { //start up clockwise
+
+};
 
 
 #endif
