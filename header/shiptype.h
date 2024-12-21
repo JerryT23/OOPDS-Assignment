cfeb07789492627669d52c1a2340139906425b2f
NCCHANG <changhoehin2005@gmail.com>
2024-12-21 17:20:08 +0800

    added all type of ship in h

--- header/shiptype.h
+++ header/shiptype.h
@@ -1,49 +1,68 @@
 #ifndef SHIPTYPE_H
 #define SHIPTYPE_H
-class Ship {
+class Ship
+{
     int shipPositionX;
     int shipPositionY;
-    public:
+    int life = 3;
+
+public: // find a virtual func for base
+    virtual ~Ship() {}
+};
+
+// no overlapping method and attribute for subclasses
+
+class SeeingRobot : public Ship
+{
+public:
     virtual void look() = 0;
+    virtual ~SeeingRobot() {}
+};
+
+class MovingShip : public Ship
+{
+public:
     virtual void move() = 0;
+    virtual ~MovingShip() {}
+};
+
+class ShootingShip : public Ship
+{
+public:
     virtual void shoot() = 0;
+    virtual ~ShootingShip() {}
+};
+
+class RamShip : public Ship
+{
+public:
     virtual void ram() = 0;
+    virtual ~RamShip() {}
 };
 
-//no overlapping method and attribute for subclasses
 
-class SeeingRobot : public Ship {
-   void look() {
-    //implement look
-   }
+class Battleship : public MovingShip, public SeeingRobot, public ShootingShip
+{
 };
 
-class MovingShip : public Ship {
-   //implement look
+class Cruiser : public SeeingRobot, public MovingShip, public RamShip
+{
 };
 
-class ShootingShip : public Ship {
-    //implement look
+class Destroyer : public MovingShip, public SeeingRobot, public ShootingShip, public RamShip
+{
 };
-
-class RamShip : public Ship {
-    //implement look
+class Frigate : public ShootingShip
+{ // start up clockwise
 };
-
-class Battleship : public MovingShip, public SeeingRobot, public ShootingShip{
-
+class Corvette : public ShootingShip
+{ // immediate nearby random
 };
-
-class Cruiser : public SeeingRobot, public MovingShip, public RamShip {
-
-};
-
-class Destroyer : public MovingShip, public SeeingRobot, public ShootingShip, public RamShip {
-
-};
-class Frigate : public ShootingShip, { //start up clockwise
-
-};
-
+class Amphibious : public MovingShip, public SeeingRobot, public ShootingShip {
+    //can walk land & water
+} ;
+class Supership : public SeeingRobot, public MovingShip, public RamShip {
+    //
+} ;
 
 #endif
