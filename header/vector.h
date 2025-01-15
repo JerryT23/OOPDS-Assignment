201b8a0f27c73518598432ed35b8933ff6375a5e
NCCHANG <changhoehin2005@gmail.com>
2025-01-15 22:42:13 +0800

    vector clear

--- header/vector.h
+++ header/vector.h
@@ -4,8 +4,8 @@ template <typename T> class Vector
 {
 private:
     T *arr;
-    int size = 1;
-    int element = 0;
+    int capacity = 1;
+    int size = 0;
 
 public:
     Vector()
@@ -18,24 +18,24 @@ public:
     }
     void push_back(T item)
     {
-        if (size == element)
+        if (capacity == size)
         {
             T *temp;
-            size *= 2;
-            temp = new T[size];
-            for (int i = 0; i < (size / 2); i++)
+            capacity *= 2;
+            temp = new T[capacity];
+            for (int i = 0; i < (capacity / 2); i++)
             {
                 temp[i] = arr[i];
             }
             delete[] arr;
             arr = temp;
         }
-        arr[element] = item;
-        element += 1;
+        arr[size] = item;
+        size += 1;
     }
     T operator[](int i)
     {
-        if (i >= element)
+        if (i >= size)
         {
             std::cout << "ACCESS VIOLATION, OUT OF RANGE! CONSIDER ENDING THE PROGRAM" << std::endl;
             exit(-1);
@@ -43,7 +43,13 @@ public:
         return arr[i];
     }
     int get_size() {
-        return element;
+        return size;
+    }
+    void clear() {
+        if(arr) delete[] arr;
+        arr = new T[1];
+        capacity = 1;
+        size = 0;
     }
 };
 #endif
