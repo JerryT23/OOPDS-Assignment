16ff8bba8e9ce749b1d0920213ff6bc16ca6e5d6
NCCHANG <changhoehin2005@gmail.com>
2025-01-16 13:55:26 +0800

    close outputfile

--- src/game.cpp
+++ src/game.cpp
@@ -231,25 +231,25 @@ void Game::start() {
     int teamI = 0;
     int shipI = 0;
     Node* shipPtr = teams[teamI].getLinkedListHead(); //get first team head
-    for(int i = 0; i < iterations; i++) {//iteration
-        if(!shipPtr) { //move to next team after all ships done
-            teamI++;
-            shipI = 0;
-            if(teamI == teamShipTotal.get_size()) //if teamI out of range reset back to zero
-                teamI = 0;
-            shipPtr = teams[teamI].getLinkedListHead();
-        }
-        shipPtr->value->action(grid,teams[teamI].getShipPosition(shipI)[0],teams[teamI].getShipPosition(shipI)[1],
-                            width,height); //action
-        printGrid();
-        shipPtr = shipPtr->next;
-        shipI++;
-    }
+    // for(int i = 0; i < iterations; i++) {//iteration
+    //     if(!shipPtr) { //move to next team after all ships done
+    //         teamI++;
+    //         shipI = 0;
+    //         if(teamI == teamShipTotal.get_size()) //if teamI out of range reset back to zero
+    //             teamI = 0;
+    //         shipPtr = teams[teamI].getLinkedListHead();
+    //     }
+    //     shipPtr->value->action(grid,teams[teamI].getShipPosition(shipI)[0],teams[teamI].getShipPosition(shipI)[1],
+    //                         width,height); //action
+    //     printGrid();
+    //     shipPtr = shipPtr->next;
+    //     shipI++;
+    // }
     
     //--------------------------------- testing
-    // cout << endl;
-    // shipPtr->value->action(grid,teams[0].getShipPosition(shipI)[0],teams[teamI].getShipPosition(shipI)[1],
-    // width,height);
-    // printGrid();
+    cout << endl;
+    shipPtr->value->action(grid,teams[0].getShipPosition(shipI)[0],teams[teamI].getShipPosition(shipI)[1],
+    width,height);
+    printGrid();
     //----------------------------------
 }
